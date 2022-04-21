8163b16ede86961e25f32ef04bf66186b22fc585
Hittau <74827598+Hittau@users.noreply.github.com>
2022-04-21 20:24:43 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        main: '#CF404D',
+        dark: '#313131',
+      },
+      fontFamily: {
+        'Oswald': ['Oswald, sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
